17296ead4de2b459c3a031041d3724d81e48f295
Swapnil-M1D62Y402 <148000541+Swapnil-M1D62Y402@users.noreply.github.com>
2025-03-31 19:04:37 +0530

    added dashboard template

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import DashboardComponent from "@/components/dashboard"
 const dashboard = () => {
   return (
     <div>
-      This is Dashboard Page
+      <DashboardComponent />
     </div>
   )
 }
